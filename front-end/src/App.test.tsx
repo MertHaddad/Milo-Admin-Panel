ee5bce6076968c7df8ec5605e23f00d09a09b492
Mert Haddad <82902696+MertHaddad@users.noreply.github.com>
2022-08-28 20:46:08 +0300

    initial comment

---
+++ front-end/src/App.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Check if MAP word exists', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/(MAP)/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('background color should be dark blue', () => {
+  render(<App />);
+
+  const app = screen.getByTestId("header")
+  expect(app).not.toHaveStyle(`backgroundColor:#282c34`);
+})
